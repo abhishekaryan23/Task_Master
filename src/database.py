d61c061ec920f2a3b55a276821cf3738b93e1626
abhishekaryan23 <46811499+abhishekaryan23@users.noreply.github.com>
2023-05-09 14:01:49 +0530

    streamlit share hosting

--- src/database.py
+++ src/database.py
@@ -4,14 +4,14 @@ import sys
 import streamlit as st
 import toml
 
-# Load the contents of the config.toml file
-config = toml.load('config.toml')
+# # Load the contents of the config.toml file
+# config = toml.load('config.toml')
 
-# Access the MongoDB URI from the loaded config
-MONGO_URI = config['mongodb']['uri']
+# # Access the MongoDB URI from the loaded config
+# MONGO_URI = config['mongodb']['uri']
 
 # if you want to host your app on streamlit share uncomment below line and comment the above 2 lines.
-# MONGO_URI = st.secrets['MONGO_URI']
+MONGO_URI = st.secrets['MONGO_URI']
 
 client = MongoClient(MONGO_URI)
 db = client.task_management
